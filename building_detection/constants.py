3255d46ceddc891919c01046f671364d4d441269
splanzer <splanzer@linz.govt.nz>
2021-03-25 15:24:41 +1300

    docs: update readme

--- building_detection/constants.py
+++ building_detection/constants.py
@@ -2,13 +2,38 @@
 Configuration for building detection pipeline
 """
 
-TRAIN_IDS = ["BG43_5000_0709_RGBI"]  # , "BG43_5000_0609", "BG43_5000_0710"]
+TRAIN_IDS = [
+    "BD45_5K_0502",
+    "BD45_5K_0503",
+    "BD45_5K_0605",
+    "BD45_5K_1003",
+    "BE43_5K_0910",
+    "BE44_5K_0110",
+    "BE44_5K_0408",
+    "BE44_5K_0610",
+    "BE44_5K_0810",
+    "BE45_5K_0303",
+    "BE45_5K_0403",
+    "BE45_5K_1001",
+    "BF42_5K_0601",
+    "BF42_5K_0701",
+    "BF43_5K_1002",
+    "BF44_5K_0110",
+    "BF44_5K_0808",
+    "BG41_5K_0905",
+    "BG43_5K_0404",
+    "BG43_5K_0503",
+    "BG43_5K_0506",
+    "BG43_5K_0507",
+    "BG43_5K_0606",
+    "BG43_5K_0607",
+    "BG43_5K_0608",
+    "BG43_5K_0708",
+    "BG43_5K_0710",
+]
 
-VALID_IDS = ["BG43_5000_0507_RGBI"]  # , "BG43_5000_0508"]
+VALID_IDS = ["BD45_5K_0701", "BE44_5K_1010", "BE44_5K_0310", "BD45_5K_0907", "BG43_5K_0709", "BG43_5K_0609"]
 
-# TRAIN_IDS = ["BG32_5K_0708_TRAIN"]
-
-# VALID_IDS = ["BG32_5K_0708_VAL"]
 
 CHIP_SIZE = 300
 
@@ -16,7 +41,7 @@ CHIP_SIZE = 300
 CHANNEL_ORDER = [0, 1, 2]
 
 # Segmentation Config
-NUM_EPOCHS = 50
+NUM_EPOCHS = 54
 TEST_NUM_EPOCHS = 1
-BATCH_SIZE = 8
+BATCH_SIZE = 8  # mem error at 32
 LEARNING_RATE = 1e-4
