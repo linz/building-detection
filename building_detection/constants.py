a5b3f2d8608a19825f403db03e6d95620feedf3c
splanzer <splanzer@linz.govt.nz>
2021-07-06 14:11:53 +1200

    feat: upgrade to rv 0.13

--- building_detection/constants.py
+++ building_detection/constants.py
@@ -1,47 +1,65 @@
 """
 Configuration for building detection pipeline
+
+USAGE:
+rastervision -p batch run batch building_detection/building_detection.py \
+    train predict eval bundle \
+    -a labels_uri 's3://building-detection-data' \
+    -a processed_uri 's3://building-detection-data/output/data/processed' \
+    -a root_uri 's3://building-detection-data/output'\
+    -a multiband False
 """
-
-TRAIN_IDS = [
-    "BD45_5K_0502",
-    "BD45_5K_0503",
-    "BD45_5K_0605",
-    "BD45_5K_1003",
-    "BE43_5K_0910",
-    "BE44_5K_0110",
-    "BE44_5K_0408",
-    "BE44_5K_0610",
-    "BE44_5K_0810",
-    "BE45_5K_0303",
-    "BE45_5K_0403",
-    "BE45_5K_1001",
-    "BF42_5K_0601",
-    "BF42_5K_0701",
-    "BF43_5K_1002",
-    "BF44_5K_0110",
-    "BF44_5K_0808",
-    "BG41_5K_0905",
-    "BG43_5K_0404",
-    "BG43_5K_0503",
-    "BG43_5K_0506",
-    "BG43_5K_0507",
-    "BG43_5K_0606",
-    "BG43_5K_0607",
-    "BG43_5K_0608",
-    "BG43_5K_0708",
-    "BG43_5K_0710",
-]
-
-VALID_IDS = ["BD45_5K_0701", "BE44_5K_1010", "BE44_5K_0310", "BD45_5K_0907", "BG43_5K_0709", "BG43_5K_0609"]
 
 
 CHIP_SIZE = 300
 
 # NIR-RGB
-CHANNEL_ORDER = [0, 1, 2]
+CHANNEL_ORDER = [0, 1, 2, 3]
 
 # Segmentation Config
-NUM_EPOCHS = 54
-TEST_NUM_EPOCHS = 1
+NUM_EPOCHS = 3
+TEST_NUM_EPOCHS = 4
 BATCH_SIZE = 8  # mem error at 32
 LEARNING_RATE = 1e-4
+
+# Path configurations
+image_datasets = {
+    "waikato_rural_2017-19_0.3m": {
+        "s3_uri": "s3://linz-raster-data-store",
+        "path": "aerial-imagery/waikato_rural_2017-19_0.3m",
+        "file_type": "tif",
+    },
+    "christchurch_urban_2015-2016": {
+        "s3_uri": "s3://linz-data-lake-raster-prod",
+        "path": "aerial-imagery/new-zealand/christchurch_urban_2015-2016_0.075m_RGB",
+        "file_type": "tif",
+    },
+}
+
+# Training and validation data
+DATA = {
+    "training": {
+        "BB32_5000_0906": {
+            "s3_uri": image_datasets["waikato_rural_2017-19_0.3m"]["s3_uri"],
+            "path": image_datasets["waikato_rural_2017-19_0.3m"]["path"],
+            "file_type": image_datasets["waikato_rural_2017-19_0.3m"]["file_type"],
+        },
+        "BB32_5000_1007": {
+            "s3_uri": image_datasets["waikato_rural_2017-19_0.3m"]["s3_uri"],
+            "path": image_datasets["waikato_rural_2017-19_0.3m"]["path"],
+            "file_type": image_datasets["waikato_rural_2017-19_0.3m"]["file_type"],
+        },
+        "BW23_500_098097": {
+            "s3_uri": image_datasets["christchurch_urban_2015-2016"]["s3_uri"],
+            "path": image_datasets["christchurch_urban_2015-2016"]["path"],
+            "file_type": image_datasets["christchurch_urban_2015-2016"]["file_type"],
+        },
+    },
+    "validation": {
+        "BC33_5000_1005": {
+            "s3_uri": image_datasets["waikato_rural_2017-19_0.3m"]["s3_uri"],
+            "path": image_datasets["waikato_rural_2017-19_0.3m"]["path"],
+            "file_type": "tif",  # this is lower case. most of the dataset is upper :(
+        }
+    },
+}
